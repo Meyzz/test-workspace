1d320713e6260cbb03d520f4edfb835be7221964
Litvinov Andrey <drune@gopoints.ru>
2020-09-10 20:56:04 +0300

    just small fixes in gitignore and remove npm lock files

--- components/src/index.ts
+++ components/src/index.ts
@@ -1,14 +1,2 @@
-import styled from 'styled-components';
-
-export const sum = (a: number, b: number) => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('boop');
-  }
-  return a + b;
-};
-
-export const Container = styled.div`
-    background: lightgray;
-    width: 50px;
-    height: 50px;
-    `;
+export * from './Container';
+export * from './sum';
